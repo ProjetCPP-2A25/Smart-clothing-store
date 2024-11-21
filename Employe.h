16f7472a2d20cc706b4377179628b7b0f321c649
Asma <asma.messaoudi@esprit.tn>
2024-11-21 20:21:42 +0100

    frist commit employe.h

---
+++ Employe.h
@@ -0,0 +1,76 @@
+#ifndef EMPLOYE_H
+#define EMPLOYE_H
+
+#include <QMainWindow>
+#include <QObject>
+#include <QSharedDataPointer>
+#include <QWidget>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+#include <QString>
+#include <QDate>
+
+class Employe
+{
+public:
+    // Constructors
+    Employe();
+    Employe(int, QString, QString, QString, QString, int, QString,QString,QDate ,QDate ,int);
+    static Employe GetByCIN(int CIN); // Declaration of the method
+
+    // Getters
+    int getCIN();
+    QString getNOM_E();
+    QString getPRENOM_E();
+    QString getPOST();
+    QString getEMAIL();
+    int getTELEPHONE();
+    QString getADRESSE();
+    QString getLEAVE_TYPE();
+    QDate getFIRST_DAY_OF_LEAVE();
+    QDate getLAST_DAY_OF_LEAVE();
+    int getNBDAYS();
+
+    // Setters
+    void setCIN(int);
+    void setNOM_E(QString);
+    void setPRENOM_E(QString);
+    void setPOST(QString);
+    void setEMAIL(QString);
+    void setTELEPHONE(int);
+    void setADRESSE(QString);
+    void setLEAVE_TYPE(QString LEAVE_TYPE);
+    void setFIRST_DAY_OF_LEAVE(QDate date);
+    void setLAST_DAY_OF_LEAVE(QDate date);
+    void setNBDAYS(int nbdays);
+
+    // Functions
+    bool ajouter();
+    bool isValidEmail(const QString &email);
+
+    QSqlQueryModel* afficherEMP();
+    QSqlQueryModel* afficherCONG();
+    //QSqlQueryModel* afficherfiltre();
+    bool supprimer(int);
+    bool cinexiste(int CIN);
+    /*bool Modifier(QString NOM_E, QString PRENOM_E, QString POST, QString EMAIL, int TELEPHONE, QString ADRESSE, QString LEAVE_TYPE, QDate FIRST_DAY_OF_LEAVE,
+                  QDate LAST_DAY_OF_LEAVE,int NBDAYS);*/
+    bool Modifier();
+    bool supprimerCONG(int CIN);
+
+    void stat_bateau();
+
+    QSqlQueryModel*afficherCon();
+
+private:
+    int CIN;
+    QString NOM_E, PRENOM_E, POST, EMAIL;
+    int TELEPHONE;
+    QString ADRESSE;
+    QString LEAVE_TYPE;
+    QDate FIRST_DAY_OF_LEAVE;
+    QDate LAST_DAY_OF_LEAVE;
+     int NBDAYS;
+};
+
+#endif // EMPLOYE_H
